8a50dd79709ca68d295e5b0032116c2130ea9627
Kevin Garner <kevin@kgar.net>
2020-12-25 20:13:11 -0600

    added recover and split files

---
+++ src/recover.rs
@@ -0,0 +1,4 @@
+// recover.rs
+// Author: Kevin Garner, kevin@kgar.net
+//
+// Functions used to recover secret file from shares
---
+++ src/split.rs
@@ -0,0 +1,6 @@
+// split.rs
+// Author: Kevin Garner, kevin@kgar.net
+//
+// Functions used to split file into multiple shares
+
+
