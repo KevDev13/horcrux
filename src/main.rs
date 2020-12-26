8f232f7deadf90d269141ffb48f92a9de22aff8b
Kevin Garner <kevin@kgar.net>
2020-12-25 20:19:45 -0600

    changed maximum shares to total shares

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,9 @@ fn main() {
     println!("{:?}", str::from_utf8(&secret).unwrap());
 }
 
-fn get_shares(minimum: &String, maximum: &String) -> Option<(u8, usize)> {
+fn get_shares(minimum: &String, total: &String) -> Option<(u8, usize)> {
     let minimum_as_number = minimum.parse::<i32>().expect("Minimum shares not an integer");
-    let maximum_as_number = maximum.parse::<usize>().expect("Maximum shares not an integer");
+    let total_as_number = total.parse::<usize>().expect("Total shares not an integer");
 
     if minimum_as_number <= 0 {
         println!("Minimum number of shares must be a positive integer");
@@ -73,23 +73,23 @@ fn get_shares(minimum: &String, maximum: &String) -> Option<(u8, usize)> {
 
     // enforce a maximum
     const MAX_MAX: usize = 2055;
-    if maximum_as_number > MAX_MAX {
-        println!("Maximum number of shares must be less than {}", MAX_MAX+1);
+    if total_as_number > MAX_MAX {
+        println!("Total number of shares must be less than {}", MAX_MAX+1);
         return None;
     }
         
 
-    if maximum_as_number <= 0 {
-        println!("Maximum number of shares must be a positive number");
+    if total_as_number <= 0 {
+        println!("Total number of shares must be a positive number");
         return None;
     }
 
     // convert minimum to usize. Shouldn't be a problem, because by now
     // we've already verified it's between 0 and 256 exclusive.
-    if maximum_as_number <= minimum_as_number as usize {
-        println!("Maximum number of shares must be at least 1 greater than minimum number of shares");
+    if total_as_number <= minimum_as_number as usize {
+        println!("Total number of shares must be at least 1 greater than minimum number of shares");
         return None;
     }
     
-    Some((minimum_as_number as u8, maximum_as_number as usize))
+    Some((minimum_as_number as u8, total_as_number as usize))
 }
