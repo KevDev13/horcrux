d5120800f65baa1371cadc93e3b0d81cf8e5bcb7
Kevin Garner <kevin@kgar.net>
2020-12-27 19:35:42 -0600

    recovery works!

--- src/recover.rs
+++ src/recover.rs
@@ -4,10 +4,40 @@
 // Functions used to recover secret file from shares
 
 use sharks::{ Sharks, Share };
+use std::{ io::prelude::*,
+           fs::{ self, File },
+           str,
+           convert::TryFrom };
 
-pub fn recover_shares(file_name: String, shares: Vec<String>) {
+pub fn recover_shares(file_name: String, share_files: Vec<String>) {
     println!("Recovering shares into file {}", file_name);
-    // Recover the original secret!
-    //let secret = sharks.recover(shares.as_slice()).unwrap();
+
+    // read the min shares required from the header file
+    let min_data = fs::read_to_string("header.txt")
+        .expect("Unable to read header file.");
+    let min_shares: u8 = min_data.parse::<u8>()
+        .expect("Error reading header file");
+    
+    // info from all the share files
+    let mut all_shares: Vec<Vec<u8>> = Vec::new();
+
+    // read through share files and get their inputs
+    for share_file in share_files {
+        let mut inf = File::open(share_file)
+            .expect("Error in opening file");
+        let mut buffer = Vec::new();
+        inf.read_to_end(&mut buffer)
+            .expect("Error reading input");
+        all_shares.push(buffer);
+    }
+
+    let shares: Vec<Share> = all_shares.iter()
+        .map(|s| Share::try_from(s.as_slice()).unwrap()).collect();
+    // Recover the secret
+    let sharks = Sharks(min_shares);
+    let secret = sharks.recover(&shares).unwrap();
+
+    fs::write(file_name, str::from_utf8(&secret).unwrap())
+        .expect("Error writing output file");
     //println!("{:?}", str::from_utf8(&secret).unwrap());
 }
--- src/split.rs
+++ src/split.rs
@@ -4,7 +4,7 @@
 // Functions used to split file into multiple shares
 
 use sharks::{ Sharks, Share };
-use std::{ io::prelude::*, fs::File };
+use std::{ io::prelude::*, fs::{ self, File } };
 
 pub fn split_shares(file_name: String, min_shares: u8, total_shares: usize) {
     // Set a minimum threshold of shares
@@ -19,13 +19,22 @@ pub fn split_shares(file_name: String, min_shares: u8, total_shares: usize) {
              total_shares,
              min_shares);
 
+    // create main file with information required to recover
+    // this file can be distributed with all shares
+    let header_file_name = String::from("header.txt");
+    fs::write(header_file_name, min_shares.to_string().as_str())
+        .expect("Error writing to header file");
+
+    // create share fies
     let mut file_number = 1;
     for share in shares {
         let share_file_name: String = String::from("share".to_string()
                                                   + &file_number.to_string()
                                                   + &".txt".to_string());
-        let mut file = File::create(share_file_name).expect("Error in creating output file");
-        file.write_all(&Vec::from(&share)).expect("Error in writing file");
+        let mut file = File::create(share_file_name)
+            .expect("Error in creating output file");
+        file.write_all(&Vec::from(&share))
+            .expect("Error in writing file");
         file_number += 1;
     }
 }
